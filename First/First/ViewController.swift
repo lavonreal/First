2323e284fd360a3648ae2c141cb07afb65ac7b38
Joe <jyohere.d@gmail.com>
2020-05-03 20:24:12 +0900

    second line-master viewWillAppear

--- First/First/ViewController.swift
+++ First/First/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("first line-master viewWillAppear")
+        print("second line-master viewWillAppear")
     }
 }
 
